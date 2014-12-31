7349220c62da4c141b8dcd469ce7fc32da59b228
Joshua Ásberg O'Neill <joshoneill94@gmail.com>
2014-12-31 07:41:29 +0000

    Added mouse-slaved camera movement. Sped up Pulsate animation.

---
+++ Assets/Scripts/cameraMove.js
@@ -0,0 +1,50 @@
+﻿function Update() {
+
+	var mousePosX : float = Input.mousePosition.x;
+	var mousePosY : float = Input.mousePosition.y;
+	var scrollDistanceShortX : int = 800;
+	var scrollDistanceShortY : int = 400;
+	var scrollDistanceLongX : int = 500;
+	var scrollDistanceLongY : int = 200;
+	var scrollSpeedShort : float = 20;
+	var scrollSpeedLong : float = 60;
+
+	//Move Left
+	if (mousePosX < scrollDistanceShortX && mousePosX > scrollDistanceLongX)
+	{
+		transform.Translate(Vector3.right * -scrollSpeedShort * Time.deltaTime);
+	}else if (mousePosX < scrollDistanceLongX)
+	{
+		transform.Translate(Vector3.right * -scrollSpeedLong * Time.deltaTime);
+	}
+	//Move Right
+	if (mousePosX >= Screen.width - scrollDistanceShortX 
+		&& mousePosX <= Screen.width - scrollDistanceLongX)
+	{
+		transform.Translate(Vector3.right * scrollSpeedShort * Time.deltaTime);
+		
+	}else if(mousePosX >= Screen.width - scrollDistanceLongX)
+	{
+		transform.Translate(Vector3.right * scrollSpeedLong * Time.deltaTime);
+	}
+	
+	//Move Down
+	if (mousePosY < scrollDistanceShortY && mousePosY > scrollDistanceLongY)
+	{
+		transform.Translate(Vector3.up * -scrollSpeedShort * Time.deltaTime);
+	}else if (mousePosY < scrollDistanceLongY)
+	{
+		transform.Translate(Vector3.up * -scrollSpeedLong * Time.deltaTime);
+	}
+    
+    //Move Up
+	if (mousePosY >= Screen.height - scrollDistanceShortY 
+		&& mousePosY <= Screen.height - scrollDistanceLongY)
+	{
+		transform.Translate(Vector3.up * scrollSpeedShort * Time.deltaTime);
+		
+	}else if(mousePosY >= Screen.height - scrollDistanceLongY)
+	{
+		transform.Translate(Vector3.up * scrollSpeedLong * Time.deltaTime);
+	}
+}
---
+++ Assets/Scripts/triangle.js
@@ -0,0 +1,17 @@
+﻿var animator : Animator;
+
+function Start () {
+	animator = GetComponent("Animator");
+}
+
+function Update () {
+
+}
+
+function OnMouseEnter() {
+	animator.SetBool("Mouse On", true);
+}
+
+function OnMouseExit() {
+	animator.SetBool("Mouse On", false);
+}
